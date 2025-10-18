7bc7433d72805c7e3de80a37091431c36dd518ab
genekellyboyle <DrDuffy@bhhsnv.com>
2025-10-18 07:59:02 -0700

    Clean up sitemap XML structure
    - Removed unused xmlns:news namespace that may cause 400 errors
    - Kept only required namespaces: sitemap and image
    - Should resolve any XML validation issues in Google Search Console

--- app/routes/api/sitemap.tsx
+++ app/routes/api/sitemap.tsx
@@ -84,8 +84,7 @@ export async function loader() {
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
-        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
 ${allPages.map(page => `  <url>
     <loc>${baseUrl}${page.url}</loc>
     <lastmod>${currentDate}</lastmod>
