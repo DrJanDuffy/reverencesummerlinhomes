767fb7376baea79aff33ac2d5f1f165cad7e62ae
genekellyboyle <DrDuffy@bhhsnv.com>
2025-10-18 08:00:07 -0700

    Fix images sitemap XML namespace
    - Changed from image-only namespace to standard sitemap namespace with image extension
    - Uses proper xmlns:image namespace for Google Images compatibility
    - Should resolve 'Couldn't fetch' error in Google Search Console

--- app/routes/api/sitemap-images.tsx
+++ app/routes/api/sitemap-images.tsx
@@ -50,7 +50,8 @@ export async function loader() {
   ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.google.com/schemas/sitemap-image/1.1">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
 ${images.map(image => `  <url>
     <loc>${baseUrl}</loc>
     <image:image>
